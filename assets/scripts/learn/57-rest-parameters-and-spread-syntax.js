973df1b57f6cf609b648bd0ab0ca04598eec7314
b1ackitty <jieunj0301@gmail.com>
2025-07-03 15:14:49 +0900

    [학습] 유용한 JS 기능 - 전개 구문과 나머지(매개변수, 배열 나머지, 객체 나머지)

--- assets/scripts/learn/57-rest-parameters-and-spread-syntax.js
+++ assets/scripts/learn/57-rest-parameters-and-spread-syntax.js
@@ -70,4 +70,80 @@
 
   // console.log(Array.from(mainChildren))
   // console.log([...mainChildren])
-})()
+})
+
+// 나머지 매개변수(Rest Parameters)
+;(() => {
+  // function toArray(likeArrayObject) {
+  //   return Array.prototype.slice.call(likeArrayObject)
+  // }
+
+  // function logNames() {
+  //   // 인자(arguments): 매개변수의 집합
+  //   // console.log(arguments)
+
+  //   // 인자 집합은 배열이 아니다. 유사 배열 객체다.
+  //   console.log(Array.isArray(arguments)) // false
+
+  //   // 유사 배열 객체 -> 배열로 변경하기
+  //   // Array.from()
+  //   console.log(Array.from(arguments))
+  //   // 전개 구문
+  //   console.log(...arguments)
+  //   // 메서드 빌려쓰기 패턴
+  //   console.log(Array.prototype.slice.call(arguments))
+  //   // 유틸리티 함수 활용
+  //   console.log(toArray(arguments))
+
+  //   for (let i = 0, l = arguments.length; i < l; i++) {
+  //     const arg = arguments[i]
+  //     console.log(arg)
+  //   }
+  // }
+
+  // 전개 구문(...): 배열 데이터를 다른 배열 내무에 전개 -> 배열 ~ 각 원소로 펼친다.
+  // 나머지 매개변수(...): 외부에서 전달된 인수들을 하나로 모아 배열 객체로 제공
+
+  // 나머지 매개변수 활용용
+  // const logNames = (firstName, secondName, ...restNames) => {
+  const logNames = (...names) => {
+    // console.log(firstName)
+    // console.log(secondName)
+    // console.log(restNames)
+    console.log(names)
+
+    for (const name of names) {
+      console.log(name)
+    }
+  }
+
+  logNames(
+    '진형',
+    '민희',
+    '수영',
+    '진구',
+    '초영',
+    '영주',
+    '주효',
+  )
+})
+
+// 객체 합성
+;(() => {
+  /* global gsap */
+
+  const initVars = { opacity: 0, rotateX: -480 }
+
+  // gsap.timeline({ defaults: { ... } })
+
+  const commonVars = { opacity: 1, rotateX: 0 }
+  const h1Vars = { ...commonVars, scale: 1, delay: 0.5 }
+  const pVars = { ...commonVars, y: -50, color: '#551b8b', scale: 1.2, delay: 0.8 }
+
+  // console.log(Object.assign({}, commonVars, h1Vars))
+  console.log({ ...commonVars, ...h1Vars })
+
+  gsap.set('h1, p', initVars)
+  gsap.to('h1', h1Vars)
+  gsap.to('p', pVars)
+})
