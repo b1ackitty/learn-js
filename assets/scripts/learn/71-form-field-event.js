7932d22dd9c39aacc32b5467c2684a8a6e52fc19
b1ackitty <jieunj0301@gmail.com>
2025-07-10 14:44:21 +0900

    [학습] 폼 필드 이벤트
    - 인풋 요소의 이벤트 (input, change, focus, focusin, blur, focusout)
    - 체크박스 (Checkbox)

---
+++ assets/scripts/learn/71-form-field-event.js
@@ -0,0 +1,66 @@
+// --------------------------------------------------------------------------
+// 📌 폼 필드 이벤트
+// --------------------------------------------------------------------------
+
+// Form Events
+// - change
+// - input
+// - focus(버블링 ❌)
+// - focusin
+// - blur(버블링 ❌)
+// - focusout
+;(() => {
+  const form = document.querySelector('form')
+  const emailInput = document.getElementById('email')
+
+  // 폼 요소에 연결된 focus 이벤트는 이벤트 버블링되지 않음
+  // form.addEventListener('focus', ({ target }) => {
+  //   console.log('[form] focus', `<${target.localName}>`)
+  // })
+    
+  // 폼 요소에 연결된 focusin 이벤트는 이벤트 버블링됨
+  form.addEventListener('focusin', ({ target }) => {
+    console.log('[form] focusin', `<${target.localName}>`)
+  })
+
+  form.addEventListener('focusout', ({ target }) => {
+    console.log('[form] focusout', `<${target.localName}>`)
+  })
+  
+  // 인풋 요소에 연결된 change, input, focus 이벤트 감지
+  emailInput.addEventListener('change', () => console.log('[input] change event'))
+  emailInput.addEventListener('input', () => console.log('[input] input event'))
+  emailInput.addEventListener('focus', () => console.log('[input] focus event'))
+  emailInput.addEventListener('blur', () => console.log('[input] blur event'))
+})
+
+// Checkbox
+// .value
+// .checked
+;(() => {
+  const form = document.querySelector('.form')
+
+  const [ , , lemonCheckbox] = form.querySelectorAll('[type="checkbox"]')
+
+  lemonCheckbox.addEventListener('input', (e) => {
+    console.log(e.currentTarget.checked)
+  })
+
+  // form.addEventListener('change', ({ target }) => {
+  //   console.log(target.checked, target.value)
+  // })
+
+  // const checkboxes = Array.from(form.elements).filter((element) => {
+  //   return element.type === 'checkbox' && element.checked
+  // })
+  // console.log(checkboxes)
+
+  // const checkedInputs = [...form.querySelectorAll(':checked')]
+  // console.log(checkedInputs)
+
+  // for (const element of form.elements) {
+  //   console.log(element.value)
+  //   console.log(element.checked)
+  //   if (element.value === 'on') element.checked = true
+  // }
+})()
